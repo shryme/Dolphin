efdf8c7afa6de03ee35a6c4f53ea615d651bd1cd
Sébastien Paquet <shrymex@gmail.com>
2015-11-20 11:28:29 -0500

    Changed game size from 900x480 to 900x600

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     bounds: {
       width: 900,
-      height: 480
+      height: 600
       //height: 315
     },
     minWidth: 900,
-    minHeight: 480,
+    minHeight: 600,
     "resizable": false
   });
 });
