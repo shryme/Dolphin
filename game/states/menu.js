f4209acbc39a72090e28be65390b1dc4ece081b1
Sébastien Paquet <shrymex@gmail.com>
2015-11-20 11:28:57 -0500

    Removed useless button and changed the play button to a dolphin

--- game/states/menu.js
+++ game/states/menu.js
@@ -12,27 +12,14 @@ Menu.prototype = {
 		this.background.height = this.game.height;
 		this.background.width = this.game.width;
 
-		var button = this.game.add.button(this.game.world.centerX - 95, 200, 'creature_1', play, this, 2, 1, 0);
-		var button = this.game.add.button(this.game.world.centerX - 95, 300, 'creature_2', showMaze, this, 2, 1, 0);
-		var button = this.game.add.button(this.game.world.centerX - 95, 400, 'creature_3', generateMaze, this, 2, 1, 0);
-
+		var button = this.game.add.button(this.game.world.centerX - 95, 200, 'dolphin', play, this, 2, 1, 0);
     function play() {
     	this.game.state.start('play');
     }
 
-    function showMaze() {
-    	this.game.state.start('maze', true, false, true);
-    }
-
-    function generateMaze() {
-    	this.game.state.start('maze', true, false, false);
-    }
-
 	},
 	update: function() {
-		if(this.game.input.activePointer.justPressed()) {
-			// this.game.state.start('maze');
-		}
+
 	}
 };
 
