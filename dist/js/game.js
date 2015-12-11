15ee52ea4eff9a1710e910162fa0455564feb855
Sébastien Paquet <shrymex@gmail.com>
2015-12-11 15:20:25 -0500

    Removed unused images

--- dist/js/game.js
+++ dist/js/game.js
@@ -925,20 +925,12 @@ Preload.prototype = {
 
 		this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
 		this.load.setPreloadSprite(this.asset);
-		this.load.image('yeoman', 'assets/player.png');
 
 		this.load.image('menu', 'assets/background.png');
 		this.load.image('background', 'assets/background.png');
-		this.load.image('player', 'assets/player.png');
-		this.load.image('creature_1', 'assets/creature_1.png');
-		this.load.image('creature_2', 'assets/creature_2.png');
-		this.load.image('creature_3', 'assets/creature_3.png');
-		this.load.image('gameover', 'assets/background.png');
 
 		this.load.tilemap('tilemap', 'assets/tilemaps/level0.json', null, Phaser.Tilemap.TILED_JSON);
 		this.load.image('tileset', "assets/tilemaps/tileset.png");
-
-		//this.load.spritesheet('dolphin', 'assets/sprites.png', 164, 72);
 
 		this.load.atlasJSONHash('dolphin', 'assets/sprites/dolphin.png', 'assets/sprites/dolphin.json');
 		this.load.atlasJSONHash('shark', 'assets/sprites/shark.png', 'assets/sprites/shark.json');
